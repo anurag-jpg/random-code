4a332df9ecc5e15ff150d847f1113f0f66ccb957
Your Name <you@example.com>
2025-10-02 23:00:09 +0530

    update

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -5,3 +5,21 @@
 console.log(3+3)
 
 console.log("anurag")
+
+let name="anurag"
+let age=23
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean =>true/false
+//null=> standalone valuse
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof age)
+ console.log(typeof undefined)
+ console.log(typeof null)
