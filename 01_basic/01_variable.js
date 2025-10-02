5d3342bb6ef98cedf4555812ac22877a3ab39a1b
Your Name <you@example.com>
2025-10-02 22:40:28 +0530

    Initial commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=144553
+let accountEmail="deepaky9005@gmail.com"
+var accountPassword="44900263"
+accountCity="jaunpur"
+let accountState;
+
+//accountId=123
+
+accountEmail="ha@123"
+accountPassword="223"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and funcional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountState])
