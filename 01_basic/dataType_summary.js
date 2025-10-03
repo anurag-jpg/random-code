5b84b53d2ad6df9bf75152873092db3e08b5515d
Your Name <you@example.com>
2025-10-04 02:29:50 +0530

    datatypeSummary

---
+++ 01_basic/dataType_summary.js
@@ -0,0 +1,18 @@
+//Primitive
+
+// 7 Types: String,Number, Boolean, null ,Undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+//Refrence (Non primitive)
+
+// Array , Objects, Functions
