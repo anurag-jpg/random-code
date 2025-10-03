9b29d322539410d6035169e946bdc9e9021c766c
Your Name <you@example.com>
2025-10-04 01:59:16 +0530

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -19,9 +19,23 @@ console.log(booleanIsLoggedIn);
 //1=> true; 0=> false
 //"" => false
 //"anurag" => true
-
 let someNumber=33
 
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//*****************Operations**********
+
+let value=3
+
+let negValue=-value;
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
